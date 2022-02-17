21b45a2897930f19931022f578a126382361ddb2
Vory7 <72459182+Vory7@users.noreply.github.com>
2022-02-17 21:22:23 +0700

    Add files via upload

---
+++ Siaod12/Siaod12/Siaod12.cpp
@@ -0,0 +1,212 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <string>
+
+using namespace std;
+
+template<typename T>
+class List {
+public:
+    List();
+    ~List();
+
+    void pop_front();
+    void push_back(T data);
+    void clear();
+    int GetSize() { return Size; }
+    T& operator[](const int index);
+
+private:
+
+
+    template<typename T>
+    class Node {
+    public:
+        Node* pNext;
+        T data;
+
+        Node(T data = T(), Node* pNext = nullptr) {
+            this->data = data;
+            this->pNext = pNext;
+        }
+    };
+    int Size;
+    Node<T>* head;
+};
+
+template<typename T>
+List<T>::List() {
+    Size = 0;
+    head = nullptr;
+}
+
+template<typename T>
+List<T>::~List() {
+}
+
+template<typename T>
+void List<T>::pop_front()
+{
+    Node<T>* temp = head;
+    head = head->pNext;
+    delete temp;
+    Size--;
+
+
+}
+
+template<typename T>
+void List<T>::push_back(T data)
+{
+    if (head == nullptr) {
+        head = new Node<T>(data);
+    }
+    else {
+        Node<int>* current = this->head;
+        while (current->pNext != nullptr) {
+            current = current->pNext;
+        }
+        current->pNext = new Node<T>(data);
+
+    }
+    Size++;
+}
+
+template<typename T>
+void List<T>::clear()
+{
+    while (Size) {
+        pop_front();
+    }
+
+}
+
+template<typename T>
+T& List<T>::operator[](const int index)
+{
+    int counter = 0;
+    Node<T>* current = this->head;
+    while (current != nullptr) {
+        if (counter == index) {
+            return current->data;
+        }
+        current = current->pNext;
+        counter++;
+    }
+}
+
+
+
+void FillInc(List<int> lst, int n) {
+    for (int i = 0; i < n; ++i) {
+        lst.push_back(i);
+    }
+}
+
+void FillDec(List<int> lst, int n) {
+    for (int i = 0; i < n; ++i) {
+        lst.push_back(n - i);
+    }
+}
+
+void FillRand(List<int> lst, int n) {
+    srand(time(NULL));
+    for (int i = 0; i < n; ++i) {
+        lst.push_back(rand() % n + 1);
+    }
+}
+
+void MergeSort(List<int> lsta, List<int> lstb, int n) {
+    List<int> lstc;
+    int i = 0, j = 0, c = 0, CM = 0;
+    int q = lsta.GetSize(), r = lstb.GetSize();
+    while (q != 0 && r != 0) {
+        ++CM;
+        if (lsta[n - q] <= lstb[n - r]) {
+            lstc[c] = lsta[n - q];
+            ++CM;
+            --q;
+            ++c;
+        }
+        else {
+            lstc[c] = lstb[n - r];
+            ++CM;
+            --r;
+            ++c;
+        }
+    }
+    while (q > 0) {
+        lstc[c] = lsta[n - q];
+        ++CM;
+        --q;
+        ++c;
+    }
+    while (r > 0) {
+        ++CM;
+        lstc[c] = lstb[n - r];
+        --r;
+        ++c;
+    }
+    cout << CM;
+    lstc.clear();
+}
+
+int main()
+{
+    List<int> lsta, lstb;
+
+    setlocale(LC_ALL, "Russian");
+
+    int M, C, n;
+    float Ct, Mt;
+    cout << "n      M+C(t)    Dec(M+C)    Inc(M+C)    Rand(M+C)" << endl;
+    for (int i = 1; i <= 5; i++) {
+        n = 100 * i;
+
+        Ct = 2 * n - 1;
+        Mt = 2 * n;
+
+
+        if (n == 100) {
+            cout << n << "    " << Mt + Ct << "      "; FillDec(lsta, n); FillDec(lstb, n); MergeSort(lsta, lstb, n);
+            cout << "        "; FillInc(lsta, n); FillInc(lstb, n); MergeSort(lsta, lstb, n);
+            cout << "         "; FillRand(lsta, n); FillRand(lstb, n); MergeSort(lsta, lstb, n);
+            cout << endl;
+            lsta.clear(); lstb.clear();
+        }
+
+        if (n == 200) {
+            cout << n << "    " << Mt + Ct << "     "; FillDec(lsta, n); FillDec(lstb, n); MergeSort(lsta, lstb, n);
+            cout << "       "; FillInc(lsta, n); FillInc(lstb, n); MergeSort(lsta, lstb, n);
+            cout << "         "; FillRand(lsta, n); FillRand(lstb, n); MergeSort(lsta, lstb, n);
+            cout << endl;
+            lsta.clear(); lstb.clear();
+        }
+
+        if (n == 300) {
+            cout << n << "    " << Mt + Ct << "     "; FillDec(lsta, n); FillDec(lstb, n); MergeSort(lsta, lstb, n);
+            cout << "       "; FillInc(lsta, n); FillInc(lstb, n); MergeSort(lsta, lstb, n);
+            cout << "         "; FillRand(lsta, n); FillRand(lstb, n); MergeSort(lsta, lstb, n);
+            cout << endl;
+            lsta.clear(); lstb.clear();
+        }
+
+        if (n == 400) {
+            cout << n << "    " << Mt + Ct << "     "; FillDec(lsta, n); FillDec(lstb, n); MergeSort(lsta, lstb, n);
+            cout << "      "; FillInc(lsta, n); FillInc(lstb, n); MergeSort(lsta, lstb, n);
+            cout << "        "; FillRand(lsta, n); FillRand(lstb, n); MergeSort(lsta, lstb, n);
+            cout << endl;
+            lsta.clear(); lstb.clear();
+        }
+
+        if (n == 500) {
+            cout << n << "    " << Mt + Ct << "    "; FillDec(lsta, n); FillDec(lstb, n); MergeSort(lsta, lstb, n);
+            cout << "      "; FillInc(lsta, n); FillInc(lstb, n); MergeSort(lsta, lstb, n);
+            cout << "        "; FillRand(lsta, n); FillRand(lstb, n); MergeSort(lsta, lstb, n);
+            cout << endl;
+            lsta.clear(); lstb.clear();
+        }
+    }
+}
---
+++ SiaodTree0/SiaodTree0/SiaodTree0.cpp
@@ -0,0 +1,112 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+struct Vertex
+{
+	int Data;
+	string num;
+	Vertex* Left;
+	Vertex* Right;
+};
+Vertex* create();
+void Obhod(Vertex* p);
+int Size(Vertex* p);
+int Hight(Vertex* p);
+int Max(int a, int b);
+int Controlsum(Vertex* p);
+int MidLenghtRay(Vertex* p, int L);
+int main() {
+	Vertex* Root;
+	Root = create();
+	Obhod(Root);
+	cout << endl << "Size = " << Size(Root);
+	cout << endl << "Hight = " << Hight(Root);
+	cout << endl << "Midhight = " << MidLenghtRay(Root, 1) / Size(Root);
+	cout << endl << "ControllSum = " << Controlsum(Root) << endl;
+	return 0;
+}
+Vertex* create() {
+	srand(time(NULL));
+	Vertex* Root = new Vertex;
+	Vertex* p = Root;
+	p->Data = rand() % 10;
+	p->num = "1: ";
+	p->Right = NULL;
+	p->Left = new Vertex;
+	p = p->Left;
+	p->Data = rand() % 10;
+	p->num = "2: ";
+	p->Left = new Vertex;
+	p->Right = new Vertex;
+	p->Right->Data = rand() % 10;
+	p->Right->num = "2.1: ";
+	p->Right->Left = NULL;
+	p->Right->Right = NULL;
+	p = p->Left;
+	p->Data = rand() % 10;
+	p->num = "3: ";
+	p->Left = NULL;
+	p->Right = new Vertex;
+	p = p->Right;
+	p->Data = rand() % 10;
+	p->num = "4: ";
+	p->Right = NULL;
+	p->Left = new Vertex;
+	p = p->Left;
+	p->Data = rand() % 10;
+	p->num = "5: ";
+	p->Right = NULL;
+	p->Left = NULL;
+	return Root;
+}
+void Obhod(Vertex* p) {
+	if (p != NULL) {
+		cout << p->num << p->Data << endl;
+		Obhod(p->Left);
+		Obhod(p->Right);
+	}
+}
+int Size(Vertex* p) {
+	int n = 0;
+	if (p == NULL) {
+		n = 0;
+	}
+	else {
+		n = 1 + Size(p->Left) + Size(p->Right);
+	}
+	return n;
+}
+int Hight(Vertex* p) {
+	int n = 0;
+	if (p == NULL) {
+		n = 0;
+	}
+	else {
+		n = 1 + Max(Hight(p->Left), Hight(p->Right));
+	}
+	return n;
+}
+int Max(int a, int b) {
+	if (a > b) { return a; }
+	else return b;
+}
+int Controlsum(Vertex* p) {
+	int S = 0;
+	if (p == NULL) {
+		S = 0;
+	}
+	else {
+		S = p->Data + Controlsum(p->Left) + Controlsum(p->Right);
+	}
+	return S;
+}
+int MidLenghtRay(Vertex* p, int L) {
+	int S = 0;
+	if (p == NULL) {
+		S = 0;
+	}
+	else {
+		S = L + MidLenghtRay(p->Left, L + 1) + MidLenghtRay(p->Right, L + 1);
+	}
+	return S;
+}
